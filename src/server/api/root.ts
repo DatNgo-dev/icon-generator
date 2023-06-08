87a278968891bfa84c08b1875d967d4e19743295
Dat Ngo <dat_ngo10@protonmail.com>
2023-06-08 20:23:58 +0800

    Remove example.ts import

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { generateRouter } from "~/server/api/routers/generate";
 import { checkoutRouter } from "~/server/api/routers/checkout";
 
