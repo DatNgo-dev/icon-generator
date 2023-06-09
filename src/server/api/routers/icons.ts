7fda2a21b1650bcf37529da41c8390976b58e293
Dat Ngo <dat_ngo10@protonmail.com>
2023-06-09 16:19:19 +0800

    Community page for displaying icons from all users based on time creation

--- src/server/api/routers/icons.ts
+++ src/server/api/routers/icons.ts
@@ -1,14 +1,25 @@
-import { TRPCError } from "@trpc/server";
-import { z } from "zod"; // validate input
-import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 export const iconsRouter = createTRPCRouter({
   // mutation is when you modify anything on the server (Backend)
   // query is when you read (get some data from the server) anything on the server (Backend)
-  getIcons: protectedProcedure.query(({ ctx }) => {
-    const icons = ctx.prisma.icon.findMany({
+  getIcons: protectedProcedure.query(async ({ ctx }) => {
+    const icons = await ctx.prisma.icon.findMany({
       where: {
         userId: ctx.session.user.id,
+      },
+    });
+    return icons;
+  }),
+  getCommunityIcons: publicProcedure.query(async ({ ctx }) => {
+    const icons = await ctx.prisma.icon.findMany({
+      take: 50,
+      orderBy: {
+        createdAt: "desc",
       },
     });
     return icons;
